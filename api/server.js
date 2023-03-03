8fa534bc4637ffa7e63bba8b29e20d7bfeea6e50
nassersaazi <nassersnoop@gmail.com>
2023-03-03 13:59:08 +0300

    Remove sequelize sync parameter for saner model synching behavior

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync({ alter: true })
+db.sequelize.sync()
   .then(() => {
     console.log("All models were synchronised successfully.");
   })
