3b6ceb37f9ce9a7c9794a4233a78ad332aa0b18f
nully0x <40327060+nully0x@users.noreply.github.com>
2023-04-28 09:48:43 +0100

    hardcode port

--- api/server.js
+++ api/server.js
@@ -75,7 +75,7 @@ app.use(
 );
 
 // set port, listen for requests
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
