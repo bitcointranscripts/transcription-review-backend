c26bc385f32043179eb19216d88406b42ddd61f5
nully0x <40327060+nully0x@users.noreply.github.com>
2023-05-02 17:09:58 +0100

    Update index.js

--- api/app/sequelize/models/index.js
+++ api/app/sequelize/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const dbConfig = require(__dirname + '/../config/config.js')[env];
+const dbConfig = require(__dirname + '../../../config/config.js')[env];
 const db = {};
 
 const sequelize = new Sequelize(`${dbConfig.url}`, dbConfig);
