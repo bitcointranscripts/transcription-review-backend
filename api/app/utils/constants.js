b5df58a1069c0f5cf78443ad1b41cd6062f27746
Samson Olusegun <olusegunsamson@live.com>
2023-05-19 09:45:58 +0100

    refactor constants

--- api/app/utils/constants.js
+++ api/app/utils/constants.js
@@ -1,3 +1,10 @@
-export const ACTION_OPENED = 'opened';
-export const ACTION_CLOSED = 'closed';
-export const ACTION_MERGED = 'merged';
+const ACTION_OPENED = 'opened';
+const ACTION_CLOSED = 'closed';
+const ACTION_MERGED = 'merged';
+
+
+module.exports = {
+    ACTION_OPENED,
+    ACTION_CLOSED,
+    ACTION_MERGED
+}
