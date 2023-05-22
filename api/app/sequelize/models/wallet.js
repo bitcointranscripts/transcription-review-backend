6dd66dad7a3d9d3b0429d64f107511fb6734b33d
Theophilus <isahtheophilus@gmail.com>
2023-05-22 23:57:42 +0100

    feat: add wallet and transaction tables

--- api/app/sequelize/models/wallet.js
+++ api/app/sequelize/models/wallet.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.user, { foreignKey: "userId", as: "user" });
-      this.hasMany(models.transaction, { foreignKey: "walletId", as: "transactions" });
+      this.hasMany(models.transaction, {
+        foreignKey: "walletId",
+        as: "transactions",
+      });
     }
   }
   Wallet.init(
