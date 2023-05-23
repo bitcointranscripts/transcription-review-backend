88b528dddf74ea8fd04de683e8457a49cc68cfc5
Theophilus <isahtheophilus@gmail.com>
2023-05-23 11:01:21 +0100

    chore: remove comments

--- api/app/sequelize/models/user.ts
+++ api/app/sequelize/models/user.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import { Sequelize, Model, ModelStatic, DataTypes } from "sequelize";
 import { Wallet } from "./wallet";
@@ -9,7 +9,7 @@ class User extends Model {
   public id!: number;
   public githubUsername!: string;
   public authToken!: string;
-  public permissions!: 'reviewer' | 'admin';
+  public permissions!: "reviewer" | "admin";
   public archivedAt!: Date | null;
 
   // Other model attributes and methods go here
@@ -19,9 +19,9 @@ class User extends Model {
     Review: ModelStatic<Review>;
     Transcript: ModelStatic<Transcript>;
   }) {
-    this.hasOne(models.Wallet, { foreignKey: 'userId' });
-    this.hasMany(models.Review, { foreignKey: 'userId' });
-    this.hasMany(models.Transcript, { foreignKey: 'archivedBy' });
+    this.hasOne(models.Wallet, { foreignKey: "userId" });
+    this.hasMany(models.Review, { foreignKey: "userId" });
+    this.hasMany(models.Transcript, { foreignKey: "archivedBy" });
   }
 }
 
@@ -36,39 +36,14 @@ export default function initModel(sequelize: Sequelize): ModelStatic<User> {
       },
       githubUsername: DataTypes.STRING,
       authToken: DataTypes.STRING,
-      permissions: DataTypes.ENUM('reviewer', 'admin'),
+      permissions: DataTypes.ENUM("reviewer", "admin"),
       archivedAt: DataTypes.DATE,
     },
     {
       sequelize,
-      modelName: 'user',
+      modelName: "user",
     }
   );
 }
 
 export { User };
-// module.exports = (sequelize: Sequelize, DataTypes: any) => {
-//   class User extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models: { wallet: ModelStatic<Model<any, any>>; review: ModelStatic<Model<any, any>>; transcript: ModelStatic<Model<any, any>>; }) {
-//       // define association here
-//       this.hasOne(models.wallet, { foreignKey: 'userId' });
-//       this.hasMany(models.review, { foreignKey: 'userId' });
-//       this.hasMany(models.transcript, { foreignKey: 'archivedBy' });
-//     }
-//   }
-//   User.init({
-//     githubUsername: DataTypes.STRING,
-//     authToken: DataTypes.STRING,
-//     permissions: DataTypes.ENUM('reviewer', 'admin'),
-//     archivedAt: DataTypes.DATE
-//   }, {
-//     sequelize,
-//     modelName: 'user',
-//   });
-//   return User;
-// };
