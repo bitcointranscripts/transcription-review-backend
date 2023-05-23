46ee71c31bbb61d3708ac64afe9b775ec2f841b1
Emmanuel-Develops <itakpeemma@gmail.com>
2023-05-23 15:20:49 +0100

    feat: update transcript if PR is merged oe closed

--- api/app/controllers/webhook.controller.js
+++ api/app/controllers/webhook.controller.js
@@ -1,5 +1,6 @@
 const db = require("../sequelize/models");
 const Review = db.review;
+const Transcript = db.transcript;
 const { ACTION_CLOSED } = require("../utils/constants")
 
 
@@ -8,46 +9,60 @@ exports.create = async (req, res) => {
 
     //check if req.body return anything
     if (!pull_request) {
-        throw new Error("No pull request data found in the request body.");
+        res.status(500).send({
+            message: "No pull request data found in the request body."
+        })
     };
 
     const action = pull_request.action;
-    const isMerged = pull_request.pull_request.merged;
+    const isMerged = pull_request.pull_request?.merged;
+    const html_url = pull_request.pull_request?.html_url;
+    const currentTime = new Date()
+
+    // Check if the PR URL exists in the database
+    const existingReview = await Review.findOne({ where: { pr_url: html_url } });
+
+    if (!existingReview) {
+        return res.status(404).send({
+            message: `Review with pr_url=${html_url} not found`
+        });
+    }
 
     // Check if the action is closed and the PR is merged
     if (action === ACTION_CLOSED && isMerged) {
-        const html_url = pull_request.pull_request.html_url;
         try {
-            // Check if the PR URL exists in the database
-            const existingReview = await Review.findOne({ where: { pr_url: html_url } });
-            if (existingReview) {
-                // PR is merged, update the mergeAt timestamp
-                existingReview.mergedAt = new Date();
+            // PR is merged, update the mergedAt timestamp
+            existingReview.mergedAt = pull_request.pull_request.merged_at ?? currentTime;
+            await existingReview.save();
 
-                await existingReview.save();
-                res.sendStatus(200);
-            } else {
-                throw new Error(`Review with PR URL ${html_url} not found.`);
-            }
+            // find and update the associated transcript
+            const associatedTranscript = await Transcript.findByPk(existingReview.transcriptId);
+            associatedTranscript.archivedAt = currentTime
+            await associatedTranscript.save()
+
+            return res.sendStatus(200);
         } catch (error) {
-            throw new Error(error);
+            return res.status(500).send({
+                message: `Error: ${error?.message ?? "unable to update review or associated transcript"}`
+            })
         }
     } else if (action === ACTION_CLOSED && !isMerged) {
-        const html_url = pull_request.pull_request.html_url;
         try {
-            // Check if the PR URL exists in the database
-            const existingReview = await Review.findOne({ where: { pr_url: html_url } });
-            if (existingReview) {
-                // PR is merged, update the archivedAt timestamp
-                existingReview.archivedAt = new Date();
+            // PR is merged, update the archivedAt timestamp
+            existingReview.archivedAt = currentTime;
+            await existingReview.save();
 
-                await existingReview.save();
-                res.sendStatus(200);
-            } else {
-                throw new Error(`Review with PR URL ${html_url} not found.`);
-            }
+            // find and update the associated transcript
+            const associatedTranscript = await Transcript.findByPk(existingReview.transcriptId);
+            associatedTranscript.claimedBy = null;
+            associatedTranscript.status = "queued"
+            await associatedTranscript.save()
+
+            res.sendStatus(200);
         } catch (error) {
-            throw new Error(error);
+            res.status(500).send({
+                message: `Error: ${error?.message ?? "unable to update review or associated transcript"}`
+            })
         }
     } else {
         res.sendStatus(200);
