593ad21201688d67ffe9f7f151dfba4d16623a8d
Theophilus <isahtheophilus@gmail.com>
2023-05-23 11:01:21 +0100

    chore: remove comments

--- api/app/sequelize/models/transaction.ts
+++ api/app/sequelize/models/transaction.ts
@@ -1,39 +1,3 @@
-// "use strict";
-// // const { Model } = require("sequelize");
-// import { Sequelize, Model, ModelStatic } from "sequelize";
-// module.exports = (sequelize: Sequelize, DataTypes: any) => {
-//   class Transaction extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models: { wallet: ModelStatic<Model<any, any>>; }) {
-//       // define association here
-//       this.belongsTo(models.wallet, { foreignKey: "walletId" });
-//     }
-//   }
-//   Transaction.init(
-//     {
-//       walletId: DataTypes.INTEGER,
-//       amount: {
-//         type: DataTypes.DECIMAL,
-//         allowNull: false,
-//       },
-//       transactionType: {
-//         type: DataTypes.ENUM("credit", "debit", "pending"),
-//         allowNull: false,
-//       },
-//       timestamp: DataTypes.DATE,
-//     },
-//     {
-//       sequelize,
-//       modelName: "transaction",
-//     }
-//   );
-//   return Transaction;
-// };
-
 import { Sequelize, Model, ModelStatic, DataTypes } from "sequelize";
 import { Wallet } from "./wallet";
 import { Review } from "./review";
@@ -51,7 +15,7 @@ class Transaction extends Model<TransactionAttributes> {
   public walletId!: number;
   public reviewId!: number;
   public amount!: number;
-  public transactionType!: "credit" | "debit" | "pending";
+  public transactionType!: "credit" | "debit";
   public transactionStatus!: "success" | "failed" | "pending";
   public timestamp!: Date;
 
