fd6453fb8a3815da6702f59fd9c9463f5de4acf3
Theophilus <isahtheophilus@gmail.com>
2023-05-23 11:01:21 +0100

    chore: remove comments

--- api/app/sequelize/models/index.ts
+++ api/app/sequelize/models/index.ts
@@ -1,94 +1,6 @@
-// 'use strict';
-
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const process = require('process');
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const dbConfig = require(__dirname + '../../../config/config.js')[env];
-// const db = {};
-
-// const sequelize = new Sequelize(`${dbConfig.url}`, dbConfig);
-
-
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// // console.log(db)
-
-// module.exports = db;
-// Compare this snippet from api/app/sequelize/models/index.ts:
-
-// "use strict";
-
-// import fs from "fs";
-// import path from "path";
-// import { Sequelize, DataTypes } from "sequelize";
-
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || "development";
-// const dbConfig = require(__dirname + "../../../config/config.js")[env];
-// const db: any = {};
-
-// const sequelize = new Sequelize(`${dbConfig.url}`, dbConfig);
-
-// fs.readdirSync(__dirname)
-//   .filter(
-//     (file: string) =>
-//       file.indexOf(".") !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === ".ts" &&
-//       file.indexOf(".test.ts") === -1
-//   )
-//   .forEach((file: string) => {
-//     const model = require(path.join(__dirname, file)).initModel(
-//       sequelize,
-//       DataTypes
-//     );
-//     db[model.name] = model;
-//   }
-// );
-
-// Object.keys(db).forEach((modelName: string) => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// }
-// );
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// // console.log(db)
-
-// export default db;
-
-
-import { Sequelize, Model, ModelStatic } from 'sequelize';
-import path from 'path';
-import fs from 'fs';
+import { Sequelize, Model, ModelStatic } from "sequelize";
+import path from "path";
+import fs from "fs";
 const env = process.env.NODE_ENV || "development";
 const dbConfig = require(__dirname + "../../../config/config.js")[env];
 const db: any = {};
@@ -103,10 +15,15 @@ const sequelize = new Sequelize(dbConfig.url, dbConfig);
 
 let associateModelFunctions: Function[] = [];
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.ts' && file.indexOf('.test.ts') === -1)
-  .forEach(file => {
+fs.readdirSync(__dirname)
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".ts" &&
+      file.indexOf(".test.ts") === -1
+  )
+  .forEach((file) => {
     const model = require(path.join(__dirname, file));
     const sequelizeModel = model.default(sequelize);
     db[sequelizeModel.name] = sequelizeModel;
@@ -116,10 +33,9 @@ fs
   });
 
 // Once all models are imported, invoke the associate functions
-associateModelFunctions.forEach(associateFunc => associateFunc(db));
+associateModelFunctions.forEach((associateFunc) => associateFunc(db));
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 export { db, sequelize, Sequelize };
-
--- api/app/sequelize/models/wallet.ts
+++ api/app/sequelize/models/wallet.ts
@@ -1,39 +1,8 @@
 "use strict";
 
-// import { Sequelize, Model, ModelStatic } from "sequelize";
-// import { User } from "./user";
-
-
-// // const { Model } = require("sequelize");
-// module.exports = (sequelize: Sequelize, DataTypes: { INTEGER: any; }) => {
-//   class Wallet extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models: { User: typeof User; Transaction: ModelStatic<Model<any, any>>; }) {
-//       // define association here
-//       this.belongsTo(models.User, { foreignKey: "userId", as: "user" });
-//       this.hasMany(models.Transaction, { foreignKey: "walletId", as: "transactions" });
-//     }
-//   }
-//   Wallet.init(
-//     {
-//       userId: DataTypes.INTEGER,
-//       balance: { type: DataTypes.INTEGER, defaultValue: 0 },
-//     },
-//     {
-//       sequelize,
-//       modelName: "wallet",
-//     }
-//   );
-//   return Wallet;
-// };
-
-import { Sequelize, Model, DataTypes, ModelStatic } from 'sequelize';
-import { User } from './user';
-import { Transaction } from './transaction';
+import { Sequelize, Model, DataTypes, ModelStatic } from "sequelize";
+import { User } from "./user";
+import { Transaction } from "./transaction";
 
 interface WalletAttributes {
   userId: number;
@@ -50,10 +19,10 @@ class Wallet extends Model<WalletAttributes> {
     User: ModelStatic<User>;
     Transaction: ModelStatic<Transaction>;
   }) {
-    Wallet.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+    Wallet.belongsTo(models.User, { foreignKey: "userId", as: "user" });
     Wallet.hasMany(models.Transaction, {
-      foreignKey: 'walletId',
-      as: 'transactions',
+      foreignKey: "walletId",
+      as: "transactions",
     });
   }
 }
@@ -66,7 +35,7 @@ export default function initModel(sequelize: Sequelize): ModelStatic<Wallet> {
     },
     {
       sequelize,
-      modelName: 'wallet',
+      modelName: "wallet",
     }
   );
 }
