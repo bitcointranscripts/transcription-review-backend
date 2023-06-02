87735fea3237460a556ef6fc6c54f8e5afe871c0
balogunofafrica <balogunakanbi.k@gmail.com>
2023-06-02 21:45:09 +0100

    fix: controller type errors

--- api/app/sequelize/models/wallet.ts
+++ api/app/sequelize/models/wallet.ts
@@ -37,11 +37,13 @@ import { Transaction } from './transaction';
 
 interface WalletAttributes {
   userId: number;
+  id: string | number;
   balance: number;
 }
 
 class Wallet extends Model<WalletAttributes> {
   public userId!: number;
+  public id!: string | number;
   public balance!: number;
 
   // Other model attributes and methods go here
@@ -62,6 +64,12 @@ export default function initModel(sequelize: Sequelize): ModelStatic<Wallet> {
   return Wallet.init(
     {
       userId: DataTypes.INTEGER,
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
       balance: { type: DataTypes.INTEGER, defaultValue: 0 },
     },
     {
