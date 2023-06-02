80806561761e4ae189e59fb1c2975003021f99c2
balogunofafrica <balogunakanbi.k@gmail.com>
2023-06-02 23:04:38 +0100

    fix: typescript errors on controllers

--- api/app/utils/constants.ts
+++ api/app/utils/constants.ts
@@ -2,29 +2,29 @@ const PR_EVENT_ACTIONS = {
   OPENED: "opened",
   CLOSED: "closed",
   MERGED: "merged",
-};
+} as const;
 
 const TRANCRIPT_STATUS = {
   QUEUED: "queued",
   NOT_QUEUED: "not queued",
-};
+} as const;
 
 const QUERY_REVIEW_STATUS = {
   ACTIVE: "active",
   PENDING: "pending",
   INACTIVE: "inactive",
-};
+} as const;
 
 const TRANSACTION_TYPE = {
   CREDIT: "credit",
   DEBIT: "debit",
-};
+} as const;
 
 const TRANSACTION_STATUS = {
   SUCCESS: "success",
   FAILED: "failed",
   PENDING: "pending",
-};
+} as const;
 
 const SATS_REWARD_RATE_PER_WORD = 0.5;
 
