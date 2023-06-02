1b6ac1690e4f735c20703016b2c13b47d5d495b8
balogunofafrica <balogunakanbi.k@gmail.com>
2023-06-02 19:26:55 +0100

    refactor: util functions

--- api/app/utils/cron.ts
+++ api/app/utils/cron.ts
@@ -1,44 +1,54 @@
+import { Review } from "../sequelize/models/review";
+import { Transcript } from "../sequelize/models/transcript";
+// import { config } from "./config";
+
 const db = require("../sequelize/models");
-const Transcript = db.transcript
-const Review = db.review;
+
 const config = require("./utils.config");
-const { getUnixTimeFromHours } = require("../utils/review.inference")
-const CronJob = require('cron').CronJob;
+const { getUnixTimeFromHours } = require("../utils/review.inference");
+const CronJob = require("cron").CronJob;
 
 // Requeue transcript if review has expired
-function setupExpiryTimeCron (review) {
-
-  const expiryTime = new Date(review.createdAt).getTime() + getUnixTimeFromHours(config.expiryTimeInHours);
-  const expiryDate = new Date(expiryTime)
+function setupExpiryTimeCron(review: Review) {
+  const expiryTime =
+    // @ts-expect-error
+    new Date(review.createdAt).getTime() +
+    getUnixTimeFromHours(config.expiryTimeInHours);
+  const expiryDate = new Date(expiryTime);
 
   const job = new CronJob(
     expiryDate,
     async () => {
       try {
-        const thisReview = await Review.findByPk(review.id)
-        
+        const thisReview = await Review.findByPk(review.id);
+
         // don't requeue transcripts whose review has been merged or submitted
-        if (thisReview.mergedAt || thisReview.submittedAt) return;
-        
+        if (thisReview?.mergedAt || thisReview?.submittedAt) return;
+
         // TODO: archivedAt field for expired reviews
         // const now = new Date()
         // Review.update({ archivedAt: now }, {
         //   where: { id: review.id }
         // })
 
-        Transcript.update({ status: 'queued', claimedBy: null }, {
-          where: { id: review.transcriptId }
-        })
-      } catch(err) {
-        console.log(err)
+        Transcript.update(
+          { status: "queued", claimedBy: null },
+          {
+            where: { id: review.transcriptId },
+          }
+        );
+      } catch (err) {
+        console.log(err);
       }
     },
-    null, false, null,
+    null,
+    false,
+    null
   );
-  job.start()
+  job.start();
   return;
 }
 
 module.exports = {
-  setupExpiryTimeCron
-}
+  setupExpiryTimeCron,
+};
