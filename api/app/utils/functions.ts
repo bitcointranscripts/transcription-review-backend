58d0ebf84f1f6db4edd00a30c75e03e18415441f
balogunofafrica <balogunakanbi.k@gmail.com>
2023-06-02 22:14:37 +0100

    fix: implement custom word count function

--- api/app/utils/functions.ts
+++ api/app/utils/functions.ts
@@ -5,3 +5,22 @@ export const objectKeys = <T extends object, K extends keyof T>(
 export const objectValues = <T extends object, V extends T[keyof T]>(
   object: T
 ): V[] => Object.values(object) as V[];
+
+const pattern =
+  /[a-zA-Z0-9_\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff\u0400-\u04ff]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
+
+export function wordCount(data: string) {
+  var m = data.match(pattern);
+  var count = 0;
+  if (!m) {
+    return 0;
+  }
+  for (var i = 0; i < m.length; i++) {
+    if (m[i].charCodeAt(0) >= 0x4e00) {
+      count += m[i].length;
+    } else {
+      count += 1;
+    }
+  }
+  return count;
+}
