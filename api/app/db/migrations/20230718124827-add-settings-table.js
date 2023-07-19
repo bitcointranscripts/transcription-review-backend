0395dc3c0e563c29eacf8a26cf708eaa8e56d12e
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-07-19 17:20:54 +0100

    feat: migration for settings and alby token

---
+++ api/app/db/migrations/20230718124827-add-settings-table.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.renameColumn("users", "authToken", "albyToken");
+    await queryInterface.createTable("settings", {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      instantWithdraw: {
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "users", key: "id" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.renameColumn("users", "albyToken", "authToken");
+    await queryInterface.dropTable("settings");
+  },
+};
