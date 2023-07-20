dd6b1558703b2407540e545a18c8e934eae4563f
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-07-20 22:37:48 +0100

    fix: uncommented code and added decode invoice feat

--- api/app/routes/lightning.routes.ts
+++ api/app/routes/lightning.routes.ts
@@ -2,21 +2,22 @@ import type { Express } from "express";
 import express from "express";
 import * as alby from "../controllers/alby.controller";
 import * as opennode from "../controllers/opennode.controller";
+import { auth } from "../middleware/auth";
 
 export function lightningRoutes(app: Express) {
   const router = express.Router();
 
   // Save a New Alby Refresh Token
-  router.post("/alby/token", alby.saveAlbyToken);
+  router.post("/alby/token",auth, alby.saveAlbyToken);
 
   // Generate Invoice
-  router.post("/alby/invoice", alby.generateInvoice);
+  router.post("/alby/invoice",auth, alby.generateInvoice);
 
   // Generate a LN-URL withdraw QR code
-  router.post("/opennode/lnurl-withdraw", opennode.lnurlWithdrawal);
+  router.post("/opennode/lnurl-withdraw",auth, opennode.lnurlWithdrawal);
 
   // Pay a Lightning invoice
-  router.post("/opennode/invoice", opennode.lninvoiceWithdrawal);
+  router.post("/opennode/invoice",auth, opennode.lninvoiceWithdrawal);
 
   app.use("/api/lightning", router);
 }
