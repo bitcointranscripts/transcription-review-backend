69959159d1086c75d545f3b6ff86501253431999
Theophilus <isahtheophilus@gmail.com>
2023-07-24 22:31:03 +0100

    fix: sign in auth flow

--- api/app/utils/auth.ts
+++ api/app/utils/auth.ts
@@ -1,11 +1,11 @@
 
-    if (!secretKey) {
-      throw new Error("JWT_SECRET environment variable is not defined");
-    }
-    const token = jwt.sign(
-      { userId: user.id, permissions: user.permissions, githubAuthToken},
-      secretKey,
-      { expiresIn: JWTEXPIRYTIMEINHOURS}
-    );
-    return token;
+  if (!secretKey) {
+    throw new Error("JWT_SECRET environment variable is not defined");
   }
+  const token = jwt.sign(
+    { userId: user.id, permissions: user.permissions, githubAuthToken },
+    secretKey,
+    { expiresIn: JWTEXPIRYTIMEINHOURS }
+  );
+  return token;
+}
