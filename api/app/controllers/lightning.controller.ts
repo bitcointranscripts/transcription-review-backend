346cbe76da1cb6597a2dbaf6a4380c01800abf7e
Theophilus <isahtheophilus@gmail.com>
2023-07-27 00:03:19 +0100

    feat: switch LSP to voltage cloud

---
+++ api/app/controllers/lightning.controller.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from "express";
+
+import { decode } from "@node-lightning/invoice";
+
+import { Transaction, Wallet } from "../db/models";
+import { payInvoice } from "../helpers/lightning";
+import { TRANSACTION_STATUS, TRANSACTION_TYPE } from "../types/transaction";
+import { PICO_BTC_TO_SATS } from "../utils/constants";
+import { generateTransactionId } from "../utils/transaction";
+
+export async function payInvoiceController(req: Request, res: Response) {
+  const { invoice, userId } = req.body;
+  if (!invoice) {
+    return res.status(400).json({ error: "Invoice is required" });
+  }
+  if (!userId) {
+    return res.status(400).send({ message: "Please enter a valid invoice" });
+  }
+
+  const decodedInvoice = decode(invoice);
+  const amount = Number(decodedInvoice._value);
+
+  const userWallet = await Wallet.findOne({
+    where: { userId: userId },
+  });
+
+  if (!userWallet) {
+    res.status(404).send({
+      message: `Could not create transaction: wallet with for userId=${userId} does not exist`,
+    });
+    return;
+  }
+
+  const newAmount = Number(amount / PICO_BTC_TO_SATS);
+  console.log({ newAmount });
+  console.log({amount})
+  const balance = userWallet.balance;
+  if (balance < newAmount) {
+    return res.status(403).send({
+      message:
+        "You currently do not have sufficient balance to withdraw this amount",
+    });
+  }
+
+  const transactionId = generateTransactionId();
+  const transaction = {
+    id: transactionId,
+    reviewId: 1, //FIXME: reviewId is not nullable
+    amount: newAmount,
+    transactionType: TRANSACTION_TYPE.DEBIT,
+    transactionStatus: TRANSACTION_STATUS.PENDING,
+    walletId: userWallet.id,
+    timestamp: new Date(),
+  };
+
+  try {
+    const result = await Transaction.create(transaction);
+    if (!result) {
+      throw new Error("Transaction failed");
+    }
+    const response = await payInvoice(invoice);
+    if (!response) {
+      throw new Error("Payment failed");
+    }
+    await Transaction.update(
+      { transactionStatus: TRANSACTION_STATUS.SUCCESS },
+      { where: { id: transactionId } }
+    );
+    await Wallet.update(
+      { balance: balance - newAmount },
+      { where: { id: userWallet.id } }
+    );
+    res.status(200).json({ message: "Invoice paid successfully" });
+  } catch (err) {
+    Transaction.update(
+      { transactionStatus: TRANSACTION_STATUS.FAILED },
+      { where: { id: transactionId } }
+    );
+    console.error(err);
+    return res
+      .status(500)
+      .json({ error: "An error occurred. Could not pay invoice" });
+  }
+}
