3a12bcd52fd910ead6124adb13e15daa26a00aa2
Theophilus <isahtheophilus@gmail.com>
2023-07-27 00:03:19 +0100

    feat: switch LSP to voltage cloud

--- api/app/helpers/fetchInvoice.ts
+++ api/app/helpers/fetchInvoice.ts
@@ -1,8 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 import { Request } from "express";
 
+import axios, { AxiosResponse } from "axios";
+import { Request } from "express";
+
 import { AccessToken, AlbyInvoice } from "../types/lightning";
 
+const ALBY_API_URL = process.env.ALBY_API_URL;
+if (!ALBY_API_URL) {
+  throw new Error("Alby api url variable not set");
+}
 
 export async function fetchInvoice(tokens: AccessToken, req: Request) {
   try {
