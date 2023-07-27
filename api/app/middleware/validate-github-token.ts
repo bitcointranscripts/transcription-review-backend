f3cfc46bfeb535463de9136e1db32bd38359f86a
Theophilus <isahtheophilus@gmail.com>
2023-07-27 17:29:04 +0100

    chore: refactor validate github token middleware

--- api/app/middleware/validate-github-token.ts
+++ api/app/middleware/validate-github-token.ts
@@ -1,9 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
 
-import { User, Wallet } from "../db/models";
-import { generateJwtToken, verifyGitHubToken } from "../utils/auth";
-import { USER_PERMISSIONS } from "../types/user";
+import { verifyGitHubToken } from "../utils/auth";
 
 const validateGitHubToken = async (
   req: Request,
@@ -24,46 +21,11 @@ const validateGitHubToken = async (
       throw new Error();
     }
 
-    let user: User | null = null;
-    let conditions = {};
-    if (!githubUser.email) {
-      conditions = { githubUsername: githubUser.login };
-    } else {
-      conditions = { email: githubUser.email };
-    }
-
-    user = await User.findOne({
-      where: conditions,
-    });
-    if (!user) {
-      user = await User.create({
-        email: githubUser.email ?? "",
-        permissions: USER_PERMISSIONS.REVIEWER,
-        githubUsername: githubUser.login,
-      });
-
-      const walletId = uuidv4();
-      await Wallet.create({
-        userId: user.id,
-        balance: 0,
-        id: walletId,
-      });
-    }
-
-    const token = generateJwtToken(user, githubToken.toString());
-    const response = await User.update(
-      { jwt: token },
-      { where: { id: user.id } }
-    );
-
-    if (response[0] !== 1) {
-      return res.status(500).json({ error: "Failed to update user token" });
-    }
-
-    req.body.userId = user.id;
-    req.body.userPermissions = user.permissions;
+    req.body.username = githubUser?.login;
+    req.body.email = githubUser?.email;
     next();
   } catch (error) {
+    console.log(error)
     return res
       .status(403)
       .json({ error: "Failed to verify GitHub token in validate" });
