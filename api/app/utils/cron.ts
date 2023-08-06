2cd4e0e1f6539a99888e9b3650c89d84ccfe9936
Emmanuel-Develops <itakpeemma@gmail.com>
2023-08-06 23:47:40 +0100

    feat: switch cron to bull queue

--- api/app/utils/cron.ts
+++ api/app/utils/cron.ts
@@ -1,61 +1,53 @@
-import cron from "cron";
+import Queue from "bull";
 
 import { Review } from "../db/models/review";
 import { Transcript } from "../db/models/transcript";
-import { TRANSCRIPT_STATUS } from "../types/transcript";
+import { TranscriptStatus } from "../types/transcript";
 import { getUnixTimeFromHours } from "../utils/review.inference";
 import { EXPIRYTIMEINHOURS } from "./constants";
 import { deleteCache } from "../db/helpers/redis";
 import { redis } from "../db";
 
-const CronJob = cron.CronJob;
+const expiryQueue = new Queue<{reviewId: number}>("cron-for-review-expiry", {
+  defaultJobOptions: { delay: getUnixTimeFromHours(EXPIRYTIMEINHOURS) }
+})
 
 // Requeue transcript if review has expired
-async function setupExpiryTimeCron(review: Review) {
-  const expiryTime =
-    new Date(review.createdAt).getTime() +
-    getUnixTimeFromHours(EXPIRYTIMEINHOURS);
-  const expiryDate = new Date(expiryTime);
-
-  const job = new CronJob(
-    expiryDate,
-    async () => {
-      try {
-        const thisReview = await Review.findByPk(review.id);
-
-        // don't requeue transcripts whose review has been merged or submitted
-        if (thisReview?.mergedAt || thisReview?.submittedAt) return;
-
-        // TODO: archivedAt field for expired reviews
-        // const now = new Date()
-        // Review.update({ archivedAt: now }, {
-        //   where: { id: review.id }
-        // })
-
-        await Transcript.update(
-          { status: TRANSCRIPT_STATUS.QUEUED, claimedBy: null },
-          {
-            where: { id: review.transcriptId },
-          }
-        );
-        const totalItems = await Transcript.count();
-        const limit = 5;
-        const totalPages = Math.ceil(totalItems / limit);
-        for (let page = 1; page <= totalPages; page++) {
-          await deleteCache(`transcripts:page:${page}`);
-        }
-        await deleteCache(`transcript:${review.transcriptId}`);
-        await redis.srem("cachedTranscripts", review.transcriptId);
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    null,
-    false,
-    undefined
-  );
-  job.start();
-  return;
+export const addToExpiryQueue = async (reviewId: number) => {
+  await expiryQueue.add({reviewId})
 }
 
-export { setupExpiryTimeCron };
+expiryQueue.process(async (job, done) => {
+  const { reviewId } = job.data
+  try {
+    const thisReview = await Review.findByPk(reviewId);
+
+    if (!thisReview) return;
+
+    // don't requeue transcripts whose review has been merged or submitted
+    if (thisReview?.mergedAt || thisReview?.submittedAt) return;
+
+    const now = new Date()
+    thisReview.update({ archivedAt: now })
+
+    await Transcript.update(
+      { status: TranscriptStatus.queued, claimedBy: null },
+      {
+        where: { id: thisReview.transcriptId },
+      }
+    );
+    console.log("updated transcript and review")
+    const totalItems = await Transcript.count();
+    const limit = 5;
+    const totalPages = Math.ceil(totalItems / limit);
+    for (let page = 1; page <= totalPages; page++) {
+      await deleteCache(`transcripts:page:${page}`);
+    }
+    await deleteCache(`transcript:${thisReview.transcriptId}`);
+    await redis.srem("cachedTranscripts", thisReview.transcriptId);
+
+  } catch (err) {
+    console.log(err);
+  }
+  done()
+})
