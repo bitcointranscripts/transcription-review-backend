13313edc61dc9ea1fd825be32d28434ef49de00f
Theophilus <isahtheophilus@gmail.com>
2023-09-16 18:24:13 +0100

    fix: webhook credit transaction

--- api/app/db/helpers/redis.ts
+++ api/app/db/helpers/redis.ts
@@ -1,4 +1,5 @@
 import { redis } from "..";
+import { DB_QUERY_LIMIT } from "../../utils/constants";
 import { Transcript } from "../models";
 
 export const CACHE_EXPIRATION = 60 * 60 * 48; // 48 hours
@@ -7,7 +8,7 @@ export async function setCache(key: string, value: string) {
   try {
     await redis.set(key, value, "EX", CACHE_EXPIRATION, "NX");
   } catch (error: any) {
-    console.error(`Redis error: ${error.message as string}`);
+    console.error(`Redis error: set cache -> ${error.message as string}`);
   }
 }
 
@@ -15,20 +16,23 @@ export async function deleteCache(key: string) {
   try {
     const cachedData = await redis.get(key);
     if (!cachedData) {
-      throw new Error("Cache not found");
+      return;
     }
-    await redis.del(key);
-    return;
+    return await redis.del(key);
   } catch (error: any) {
-    console.error(`Redis error: ${error.message as string}`);
+    console.error(`Redis error: del cache -> ${error.message as string}`);
   }
 }
 
 export async function resetRedisCachedPages() {
   const totalItems = await Transcript.count();
-  const limit = 5;
-  const totalPages = Math.ceil(totalItems / limit);
+  const totalPages = Math.ceil(totalItems / DB_QUERY_LIMIT);
   for (let page = 1; page <= totalPages; page++) {
-    await deleteCache(`transcripts:page:${page}`);
+    const key = `transcripts:page:${page}`;
+    const cachedData = await redis.lrange(key, 0, -1);
+    if (!cachedData || cachedData.length === 0) {
+      continue;
+    }
+    await redis.del(key);
   }
 }
