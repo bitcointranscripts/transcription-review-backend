5051ce102fc1998e0b6c14de0064c182f54c34a5
kouloumos <kouloumosa@gmail.com>
2023-11-15 07:41:49 +0200

    increase maximum request body size to 1mb
    default request body size is 100kb, which is an issue for larger transcripts

--- api/server.ts
+++ api/server.ts
@@ -44,7 +44,7 @@ const specs = swaggerJsdoc(options);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   "/api/docs",
