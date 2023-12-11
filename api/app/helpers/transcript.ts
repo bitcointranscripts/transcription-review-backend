7bf2db2207391e6e2da9b049c7b27d5930dd0641
Samson Olusegun <olusegunsamson@live.com>
2023-12-11 06:18:55 +0100

    chore: complete saving in db

--- api/app/helpers/transcript.ts
+++ api/app/helpers/transcript.ts
@@ -1,6 +1,5 @@
 import crypto from "crypto";
-import { BaseParsedMdContent } from "../types/transcript";
-import * as yaml from "js-yaml";
+import  marked, {Tokens}  from "marked";
 
 const getFirstFiveWords = (paragraph: string) => {
   const words = paragraph.trim().split(/\s+/);
@@ -23,28 +22,33 @@ function generateUniqueHash(content: any) {
   return buffer.toString("base64");
 }
 
-function parseMdToJSON<T extends BaseParsedMdContent>(mdContent: string): T {
-  // This regex matches a markdown file with a YAML front matter. It captures the YAML header and the body separately.
-  const regex = /^---\n([\s\S]*?)\n---\n([\s\S]*)$/;
-  const match = mdContent.match(regex);
-
-  if (!match) {
-    throw new Error("Invalid Markdown format");
-  }
-
-  const [, header, body] = match;
-
-  let json: Record<string, string | string[] | undefined>;
-  try {
-    // Use js-yaml's safeLoad function to parse the YAML header
-    json = yaml.load(header) as Partial<T>;
-  } catch (error) {
-    throw new Error("Invalid YAML header");
-  }
-
-  json.body = body;
-
-  return json as T;
+interface JsonToken {
+  type: string;
+  raw: string;
+  text?: string;
+  tokens: JsonToken[];
 }
 
-export { generateUniqueStr, generateUniqueHash, parseMdToJSON };
+function convertTokensToJson(tokens: Tokens.ListItem[]): JsonToken[] {
+  const json: JsonToken[] = [];
+  for (const token of tokens) {
+    const jsonObject: JsonToken = {
+      type: token.type,
+      raw: token.raw,
+      text: token.text,
+      tokens: convertTokensToJson(token.tokens as Tokens.ListItem[] || []),
+    };
+    json.push(jsonObject);
+  }
+  return json;
+}
+
+function convertMdToJSON(md: string): JsonToken[] {
+  const tokens = marked.lexer(md);
+  return convertTokensToJson(tokens as Tokens.ListItem[]);
+}
+
+
+
+
+export { generateUniqueStr, generateUniqueHash, convertMdToJSON };
