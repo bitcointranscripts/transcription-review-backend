aef6742b6451478cb42f60893ab555f1759f1d28
Samson Olusegun <olusegunsamson@live.com>
2023-12-27 10:47:40 +0100

    chore: udpate alert parameters

--- api/app/helpers/sendAlert.ts
+++ api/app/helpers/sendAlert.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
 require("dotenv").config();
 
-export async function sendAlert(message: any, transcriptTitle?: string) {
+export async function sendAlert(
+  message: string,
+  transcriptTitle?: string,
+  transcriptUrl?: string,
+  transcriptHash?: string
+) {
   if (!process.env.DISCORD_WEBHOOK_URL) {
     throw new Error("DISCORD_WEBHOOK_URL is not set");
   }
 
   const time = new Date().toLocaleString();
-  const content = `New Queuer Action\nMessage: ${message}\nTranscript Title: ${transcriptTitle}\nTime: ${time}`;
+  const content = `New Queuer Action\nMessage: ${message}\nTranscript Title: ${transcriptTitle}\nTime: ${time}\nTranscriptUrl: ${transcriptUrl}\ntranscriptHash: ${transcriptHash}}`;
 
   try {
     await axios.post(process.env.DISCORD_WEBHOOK_URL, { content });
