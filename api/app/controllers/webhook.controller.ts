c2601ac6a71c1f3b18348b383f1a10588f041bc9
Samson Olusegun <olusegunsamson@live.com>
2023-12-27 10:17:50 +0100

    chore: swap email for discord alerts and cleanup

--- api/app/controllers/webhook.controller.ts
+++ api/app/controllers/webhook.controller.ts
@@ -3,10 +3,7 @@ import axios, { AxiosResponse } from "axios";
 import { Review, Transaction, Wallet, Transcript, User } from "../db/models";
 import { sequelize } from "../db";
 import { TRANSACTION_STATUS, TRANSACTION_TYPE } from "../types/transaction";
-import {
-  TranscriptAttributes,
-  TranscriptStatus,
-} from "../types/transcript";
+import { TranscriptAttributes, TranscriptStatus } from "../types/transcript";
 import { PAGE_COUNT, PR_EVENT_ACTIONS } from "../utils/constants";
 import { redis } from "../db";
 import {
@@ -16,13 +13,14 @@ import {
 import { verify_signature } from "../utils/validate-webhook-signature";
 import { generateUniqueHash, parseMdToJSON } from "../helpers/transcript";
 import { getTotalWords } from "../utils/review.inference";
-import { sendEmail } from "../helpers/email";
+import { sendAlert } from "../helpers/sendAlert";
 import {
   CACHE_EXPIRATION,
   deleteCache,
   resetRedisCachedPages,
 } from "../db/helpers/redis";
-import {BaseParsedMdContent} from "../types/transcript";
+import { BaseParsedMdContent } from "../types/transcript";
+import { json } from "sequelize";
 
 // create a new credit transaction when a review is merged
 async function createCreditTransaction(review: Review, amount: number) {
@@ -168,13 +166,18 @@ export async function create(req: Request, res: Response) {
   }
 }
 
-async function processCommit(commit: any, pushEvent: any, type: 'added' | 'modified') {
+async function processCommit(
+  commit: any,
+  pushEvent: any,
+  type: "added" | "modified"
+) {
   const changedFiles = [...commit[type]]; // get the files that were added or modified in the commit so we don't have to maintain two separate functions, this is also easily extendable when we want to take care of deleted commits.
   for (const file of changedFiles) {
     const rawUrl = `https://raw.githubusercontent.com/${pushEvent.repository.full_name}/master/${file}`;
     const response: any = await axios.get(rawUrl);
     const mdContent = response.data;
-    const jsonContent: BaseParsedMdContent = parseMdToJSON<BaseParsedMdContent>(mdContent);
+    const jsonContent: BaseParsedMdContent =
+      parseMdToJSON<BaseParsedMdContent>(mdContent);
     const transcript_by = jsonContent.transcript_by.toLowerCase();
 
     function isTranscriptValid(transcript_by: string): boolean {
@@ -204,7 +207,7 @@ async function processCommit(commit: any, pushEvent: any, type: 'added' | 'modif
       where: { transcriptHash: transcriptHash },
     });
 
-    if (existingTranscript && type === 'added') {
+    if (existingTranscript && type === "added") {
       throw new Error("transcript already exists");
     }
 
@@ -227,28 +230,34 @@ async function processCommit(commit: any, pushEvent: any, type: 'added' | 'modif
     };
 
     let transcriptData: any;
-    if (type === 'added') {
+    if (type === "added") {
       transcriptData = await Transcript.create(transcript);
-    } else if (type === 'modified') {
+    } else if (type === "modified") {
       // Find the existing transcript in the database
-      const existingTranscript = await Transcript.findOne({ where: {  transcriptUrl:rawUrl } });
-  
+      const existingTranscript = await Transcript.findOne({
+        where: { transcriptUrl: rawUrl },
+      });
+
       if (!existingTranscript) {
-        throw new Error('No transcript found to update');
+        throw new Error("No transcript found to update");
       }
-  
+
       // Update the transcript in the database
       await existingTranscript.update(transcript);
-  
+
       // Invalidate the cache
       const redisTransaction = redis.multi();
       redisTransaction.del(`transcript:${existingTranscript.id}`);
       await redisTransaction.exec();
-  
-      transcriptData = existingTranscript;
 
-      console.log(transcriptData);
+      transcriptData = existingTranscript;
     }
+
+    // Send alert to Discord
+    await sendAlert(
+      `Transcript Queued Successfully`,
+      transcriptData.originalContent.title
+    );
 
     const redisNewTranscriptTransaction = redis.multi();
 
@@ -295,15 +304,14 @@ export async function handlePushEvent(req: Request, res: Response) {
 
   try {
     for (const commit of commits) {
-      await processCommit(commit, pushEvent, 'added');
+      await processCommit(commit, pushEvent, "added");
       await processCommit(commit, pushEvent, "modified");
     }
   } catch (error) {
     // Send error email
     const message = error instanceof Error ? error.message : "Unknown error";
-    await sendEmail(message);
+    await sendAlert("Transcript Queue Error/fail", message);
     return res.status(500).json({ message: message });
   }
-
   return res.status(200).json({ message: "Transcript queued Successfully" });
 }
