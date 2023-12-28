5faa018f855a817feb6de35c5f7c3c1d869ed971
Samson Olusegun <olusegunsamson@live.com>
2023-12-28 17:04:37 +0100

    chore: implement edge cases for parser and environment

--- api/app/helpers/sendAlert.ts
+++ api/app/helpers/sendAlert.ts
@@ -3,16 +3,15 @@ require("dotenv").config();
 
 export async function sendAlert(
   message: string,
-  transcriptTitle?: string,
-  transcriptUrl?: string,
-  transcriptHash?: string
+  transcriptTitle?: string | null,
+  transcriptUrl?: string | null,
+  transcriptHash?: string | null
 ) {
   if (!process.env.DISCORD_WEBHOOK_URL) {
     throw new Error("DISCORD_WEBHOOK_URL is not set");
   }
 
-  const time = new Date().toLocaleString();
-  const content = `New Queuer Action\nMessage: ${message}\nTranscript Title: ${transcriptTitle}\nTime: ${time}\nTranscriptUrl: ${transcriptUrl}\ntranscriptHash: ${transcriptHash}}`;
+  const content = `New Queuer Action\nMessage: ${message}\nTranscript Title: ${transcriptTitle}\nTranscriptUrl: ${transcriptUrl}\ntranscriptHash: ${transcriptHash}`;
 
   try {
     await axios.post(process.env.DISCORD_WEBHOOK_URL, { content });
