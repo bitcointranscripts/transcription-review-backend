acbacdd764622ba4dbf7b2b6faf44fed2ae6fcde
nully0x <40327060+nully0x@users.noreply.github.com>
2024-01-02 14:56:08 +0100

    Update api/app/helpers/transcript.ts
    chore: switch to yaml parser package
    
    Co-authored-by: Andreas <kouloumosa@gmail.com>

--- api/app/helpers/transcript.ts
+++ api/app/helpers/transcript.ts
@@ -34,38 +34,9 @@ function parseMdToJSON<T extends BaseParsedMdContent>(mdContent: string): T {
 
   const [, header, body] = match;
 
-  const json: Record<string, string | string[] | undefined> = {} as Partial<T>;
-  const lines = header.split("\n");
-
-  for (const line of lines) {
-    const [key, ...valueParts] = line.split(":");
-    if (valueParts.length > 0) {
-      const value = valueParts.join(":").trim();
-      if (key.trim() !== "") {
-        if (value.startsWith("[") && value.endsWith("]")) {
-          try {
-            // Replace single quotes with double quotes before parsing
-            const validJson = value.replace(/'/g, '"');
-            json[key] = JSON.parse(validJson);
-          } catch (error) {
-            throw new Error("Invalid JSON format");
-          }
-        } else {
-          // Handle other key-value pairs
-          if (value.startsWith("'") && value.endsWith("'")) {
-            json[key] = value.slice(1, -1);
-          } else if (value.startsWith("'") && value.endsWith("',")) {
-            if (!json[key]) {
-              json[key] = [];
-            }
-            (json[key] as string[]).push(value.slice(1, -2));
-          } else {
-            json[key] = value;
-          }
-        }
-      }
-    }
-  }
+import yaml from "js-yaml"
+....
+const json: Record<string, string | string[] | undefined> = yaml.load(header) as Partial<T>
 
   json.body = body;
 
