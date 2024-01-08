72902e5cf6ef9a737b28baabea929d5c0454d217
Samson Olusegun <olusegunsamson@live.com>
2024-01-08 19:27:39 +0100

    feat: implement review reset for admin

--- api/app/routes/review.routes.ts
+++ api/app/routes/review.routes.ts
@@ -20,6 +20,7 @@ export function reviewRoutes(app: Express) {
    *           type: integer
    *           description: Id of the transcript that gets reviewed
    *
+   *
    *     Pagination:
    *       type: integer
    *       minimum: 1
@@ -262,7 +263,7 @@ export function reviewRoutes(app: Express) {
  *         schema:
  *           type: string
  *         required: true
- *         description: The review id
+ *         description: The transcript id
  *     responses:
  *       200:
  *         description: The review was reset successfully
