92fcdf1eeb8af6ce470a972355eed02eacca70e9
Theophilus <isahtheophilus@gmail.com>
2024-02-10 23:10:05 +0100

    fix: refactor credit transaction process in webhook controller
    This pr fixes the timeout issue when a review pr is merged in the btc transcript repo. Sometimes, the content diff is too large that the function to create the credit transaction takes longer than usual to resolve. And due to Javascript single threaded nature, it blocks the main server from processing incoming requests asynchronously until the expensive function is resolved.
    This commit proposes a hot fix (a much better should follow) which utilises NodeJs thread workers and Redis queueing system to asynchronously allocate memory on another thread separate from the main server thread to run the credit transaction process. This approach ensures the webhook response is almost instant while the transaction process is queued in the redis cache and once each transaction job is resolved, it clears from the queue and moves on to the other job.
    
    For errors that might occur during this process, the sendAlert function has been modified to accomodate transaction alerts sents to the discord bot. Only failed transaction alerts are notified as there is currently no reason to implement successful alert notification except on the user side (i.e. to user emails- should be in another pr)

--- api/app/controllers/review.controller.ts
+++ api/app/controllers/review.controller.ts
@@ -20,6 +20,7 @@ import { parseMdToJSON } from "../helpers/transcript";
 import axios from "axios";
 import { BaseParsedMdContent, TranscriptAttributes } from "../types/transcript";
 import { redis } from "../db";
+import { Logger } from "../helpers/logger";
 
 // THis function fetches and parses a transcript from a URL (already saved in the db which points to transcript on github), or returns the original transcript if no URL is provided. This is use to sync a transcript in review with the FE.
 const transcriptWrapper = async (
@@ -523,22 +524,56 @@ export const resetReviews = async (req: Request, res: Response) => {
       return res.status(404).send("Review not found");
     }
 
-    // Delete the review
-    await Review.destroy({ where: { id } });
-
     // Reset the transcript
     await Transcript.update(
       { status: "queued", claimedBy: null },
       { where: { id: review.transcriptId } }
     );
 
-    // Clear the Redis cache
+    // Get the updated transcript
+    const updatedTranscript = await Transcript.findOne({
+      where: { id: review.transcriptId },
+    });
+
+    if (!updatedTranscript) {
+      throw new Error("Transcript not found");
+    }
+    // Now you can use updatedTranscript.id
+
+    // Delete the review
+    await Review.destroy({ where: { id } });
+
+    // Clear the Redis cache for the review
     redis.del(`review:${id}`, (err, succeeded) => {
       if (err) {
         throw err;
       }
-      console.log(`Deleted review:${id} from Redis cache: ${succeeded}`);
+      Logger.info(`Redis cache cleared for review ${id}: ${succeeded}`);
     });
+
+    // Clear the Redis cache for the transcript
+    redis.del(`transcript:${updatedTranscript.id}`, (err, succeeded) => {
+      if (err) {
+        throw err;
+      }
+      Logger.info(
+        `Redis cache cleared for transcript ${updatedTranscript.id}: ${succeeded}`
+      );
+    });
+
+    // Clear the Redis cache for the pages of transcripts
+    const totalItems = await Transcript.count();
+    const totalPages = Math.ceil(totalItems / DB_QUERY_LIMIT);
+    for (let page = 1; page <= totalPages; page++) {
+      redis.del(`transcripts:page:${page}`, (err, succeeded) => {
+        if (err) {
+          throw err;
+        }
+        Logger.info(
+          `Redis cache cleared for transcripts page ${page}: ${succeeded}`
+        );
+      });
+    }
 
     res.status(200).send("Reset successful");
   } catch (err) {
