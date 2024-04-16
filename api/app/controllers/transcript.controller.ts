f96e78509672f0a1e1f6624930125f78b1709596
Theophilus <isahtheophilus@gmail.com>
2024-04-16 13:12:37 +0100

    refactor: pending reviews to apply different pending limits
    This commit checks if a user has successfully reviewed fewer than 3 transcripts.
    If true, we allow the user to claim only 1 transcript and return an error if user has already has a pending review.
    If the user has successfully reviewed 3 or more transcripts, we allow to have 6 pending reviews.
    If the user has 6 or more pending reviews, we return an error.

--- api/app/controllers/transcript.controller.ts
+++ api/app/controllers/transcript.controller.ts
@@ -9,7 +9,11 @@ import {
   buildIsPendingCondition,
   getTotalWords,
 } from "../utils/review.inference";
-import { DB_QUERY_LIMIT, MAXPENDINGREVIEWS } from "../utils/constants";
+import {
+  DB_QUERY_LIMIT,
+  MAX_PENDING_REVIEWS,
+  MERGED_REVIEWS_THRESHOLD,
+} from "../utils/constants";
 import { generateUniqueHash } from "../helpers/transcript";
 import { redis } from "../db";
 import {
@@ -347,7 +351,7 @@ export async function claim(req: Request, res: Response) {
   const transcriptId = req.params.id;
 
   const uid = req.body.claimedBy;
-  const branchUrl = req.body.branchUrl
+  const branchUrl = req.body.branchUrl;
   const currentTime = new Date().getTime();
   const activeCondition = buildIsActiveCondition(currentTime);
   const pendingCondition = buildIsPendingCondition();
@@ -366,12 +370,28 @@ export async function claim(req: Request, res: Response) {
     return;
   }
 
+  // if user has successfully reviewed fewer than 3 transcripts
+  // allow to claim only 1 transcript and return if user has already has a pending review
+  // if user has successfully reviewed 3 or more transcripts, allow to have 6 pending reviews
+  const successfulReviews = await Review.findAll({
+    where: { ...userCondition, mergedAt: { [Op.ne]: null } },
+  });
   const pendingReview = await Review.findAll({
     where: { ...userCondition, ...pendingCondition },
   });
-  if (pendingReview.length >= MAXPENDINGREVIEWS) {
+  if (
+    successfulReviews.length <= MERGED_REVIEWS_THRESHOLD &&
+    pendingReview.length
+  ) {
     res.status(500).send({
-      message: "User has too many pending reviews, clear some and try again!",
+      message:
+        "You have a pending review, finish it first before claiming another!",
+    });
+    return;
+  }
+  if (pendingReview.length >= MAX_PENDING_REVIEWS) {
+    res.status(500).send({
+      message: `You have ${pendingReview.length} pending reviews, clear some and try again!`,
     });
     return;
   }
@@ -382,7 +402,7 @@ export async function claim(req: Request, res: Response) {
   };
 
   if (branchUrl) {
-    review.branchUrl = branchUrl
+    review.branchUrl = branchUrl;
   }
 
   try {
