20f52f03dfcc17fc22337f62bc31710de03e5a40
0tuedon <90271995+0tuedon@users.noreply.github.com>
2026-08-15 19:02:38 +0100

    fix: used lntbs for tmp signet check

--- api/app/controllers/lightning.controller.ts
+++ api/app/controllers/lightning.controller.ts
@@ -15,7 +15,7 @@ export async function payInvoiceController(req: Request, res: Response) {
     return res.status(400).json({ error: "Invoice is required" });
   }
 
-  const prefix =   process.env.NODE_ENV === "production" ? "lnbc" : "lnsb";
+  const prefix = process.env.NODE_ENV === "production" ? "lnbc" : "lntbs";
   if (!invoice.startsWith(prefix)) {
     if (invoice.includes("@")) {
       return res.status(400).send({
